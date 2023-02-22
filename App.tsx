256f6235251c80e786cbc56476f02eae3594b240
Felix Nabucodonosor Peña <nabucopena@gmail.com>
2023-02-22 13:23:36 -0300

    Location

--- App.tsx
+++ App.tsx
@@ -5,36 +5,16 @@
  * @format
  */
 
-import * as React from 'react';
-import {Text, Button, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, Button, View, PermissionsAndroid} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Geolocation from 'react-native-geolocation-service';
+
 
 const Stack = createNativeStackNavigator();
 
 
-
-function App(): JSX.Element {
-
-  const [clicks, setClicks] = useState(0)
-
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text> Clicks: {clicks} </Text>
-      <Button
-        onPress={()=> {
-          setClicks(clicks + 1);
-        }}
-        title="Click me!"
-      />
-    </View>
-  );
-}
 
 const HomeScreen = ({navigation}) => {
   return (
@@ -48,6 +28,12 @@ const HomeScreen = ({navigation}) => {
         navigation.navigate("Profile", {name: "Nabuco"})
       }
     />
+    <Button
+          title="Location"
+          onPress={() =>
+            navigation.navigate("Location")
+          }
+        />
     </View>
   );
 }
@@ -55,6 +41,95 @@ const HomeScreen = ({navigation}) => {
 const ProfileScreen = ({navigation, route}) => {
   return <Text>This is {route.params.name}'s profile</Text>;
 };
+
+
+
+{/* Location */}
+
+
+
+
+const requestLocationPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Geolocation Permission',
+        message: 'Can we access your location?',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    console.log('granted', granted);
+    if (granted === 'granted') {
+      console.log('You can use Geolocation');
+      return true;
+    } else {
+      console.log('You cannot use Geolocation');
+      return false;
+    }
+  } catch (err) {
+    return false;
+  }
+};
+
+
+
+const Location = ({navigation}) => {
+
+  const [location, setLocation] = useState(false);
+
+  const getLocation = () => {
+    const result = requestLocationPermission();
+    result.then(res => {
+      console.log('res is:', res);
+      if (res) {
+        Geolocation.getCurrentPosition(
+          position => {
+            console.log(position);
+            setLocation(position);
+          },
+          error => {
+            // See error code charts below.
+            console.log(error.code, error.message);
+            setLocation(false);
+          },
+          {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+        );
+      }
+    });
+    console.log(location);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Welcome!</Text>
+      <View
+        style={{marginTop: 10, padding: 10, borderRadius: 10, width: '40%'}}>
+        <Button title="Get Location" onPress={getLocation} />
+      </View>
+      <Text>Latitude: {location ? location.coords.latitude : null}</Text>
+      <Text>Longitude: {location ? location.coords.longitude : null}</Text>
+      <View
+        style={{marginTop: 10, padding: 10, borderRadius: 10, width: '40%'}}>
+        <Button title="Send Location" />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+   backgroundColor: '#fff',
+   alignItems: 'center',
+   justifyContent: 'center',
+ },
+});
+
+
+
 
 const Nav = () => {
   return (
@@ -66,9 +141,11 @@ const Nav = () => {
           options={{title: 'Welcome'}}
         />
         <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Location" component={Location} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-export default Nav;
+
+export default Nav
